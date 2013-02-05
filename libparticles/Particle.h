5c69fa1df38c1a8307ca9d5eb1bb6c6d588027eb
Gilad Dayagi <giladaya@yahoo.com>
2013-02-05 21:43:11 +0200

    particles class hierarchy

--- libparticles/Particle.h
+++ libparticles/Particle.h
@@ -5,26 +5,22 @@
 
 #ifndef particle_h
 #define particle_h
-#define MAX_DIM 255
+//#define MAX_DIM 255
 
 #include "Arduino.h"
 
 class Particle
 {
-	public:
-		byte x;   //left
-		byte y;   //bottom
-		signed char vx;  //horizontal velocity
-		signed char vy;  //vertical velocity
-		signed char ax;  //vertical acceleration
-		signed char ay;  //horizontal acceleration
-		byte ttl; //time to live
-		byte hue; //hue
-		boolean isAlive; //is alive?
-
-		Particle();
-		void update();
-	private:
+    public:
+        byte x;   //left
+        byte y;   //bottom
+        signed char vx;  //horizontal velocity
+        signed char vy;  //vertical velocity
+        byte ttl; //time to live
+        byte hue; //hue
+        boolean isAlive; //is alive?
+        
+        virtual void update(void);
 };
 
 #endif /* particle_h */
